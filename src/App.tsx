562269f25abd998cbe22845d8e815a3aaba24b29
Meet Rakholiya <meet@investifyd.com>
2025-07-31 08:08:43 +0530

    add redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -3,55 +3,62 @@ import { ThemeProvider } from '@/context/theme-context';
 import { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router';
+import { PersistGate } from 'redux-persist/integration/react';
 import './App.css';
 import { AuthenticatedLayout } from './components/layout/authenticated-layout';
 import Loading from './components/Loading';
 import { Toaster } from './components/ui/sonner';
 import NotFoundError from './pages/Errors/not-found-error';
 import { ProtectedRoutes, PublicRoutes } from './router';
-import { store } from './store/store';
+import { persistor, store } from './store/store';
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <FontProvider>
-          {/* <RouterProvider router={router} /> */}
-          <Routes>
-            {ProtectedRoutes.map((page) => (
-              <Route
-                key={page.path}
-                path={page.path}
-                element={
-                  <AuthenticatedLayout>
-                    <Suspense fallback={<Loading />}>{page.component}</Suspense>
-                  </AuthenticatedLayout>
-                }
-              />
-            ))}
-            <Route>
-              {PublicRoutes.map((page) => (
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <FontProvider>
+            {/* <RouterProvider router={router} /> */}
+            <Routes>
+              {ProtectedRoutes.map((page) => (
                 <Route
                   key={page.path}
                   path={page.path}
                   element={
-                    <Suspense fallback={<Loading />}>{page.component}</Suspense>
+                    <AuthenticatedLayout>
+                      <Suspense fallback={<Loading />}>
+                        {page.component}
+                      </Suspense>
+                    </AuthenticatedLayout>
                   }
                 />
               ))}
-            </Route>
-            <Route
-              path="*"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <NotFoundError />
-                </Suspense>
-              }
-            />
-          </Routes>
-          <Toaster duration={5000} richColors />
-        </FontProvider>
-      </ThemeProvider>
+              <Route>
+                {PublicRoutes.map((page) => (
+                  <Route
+                    key={page.path}
+                    path={page.path}
+                    element={
+                      <Suspense fallback={<Loading />}>
+                        {page.component}
+                      </Suspense>
+                    }
+                  />
+                ))}
+              </Route>
+              <Route
+                path="*"
+                element={
+                  <Suspense fallback={<Loading />}>
+                    <NotFoundError />
+                  </Suspense>
+                }
+              />
+            </Routes>
+            <Toaster duration={5000} richColors />
+          </FontProvider>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from '.';
-// ...
+
+// Define the root state type
+export type RootState = ReturnType<typeof rootReducer>;
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer as any);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store);
+
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
